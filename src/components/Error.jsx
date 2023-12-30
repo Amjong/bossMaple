7e544fb96e9fb33f8632f324ec364f9a77beed38
Jongbum <amjong2@gmail.com>
2023-12-30 20:03:37 +0900

    components: Modify style of some components(error, loading)

---
+++ src/components/Error.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Error() {
+  return (
+    <div className='text-white text-3xl text-center'>
+      Something went wrong...
+    </div>
+  );
+}
