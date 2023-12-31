a3a10818c9882dcd7c34a3a19b137166e6a015a7
Jongbum <amjong2@gmail.com>
2023-12-31 19:51:25 +0900

    src/util/statApi.js : Add getCombatPower api (parsing combatpower from stat info)

--- src/components/CharacterTable.jsx
+++ src/components/CharacterTable.jsx
@@ -1,11 +1,15 @@
 import useCharacterStatQuery from '../service/hooks/useChacterStatQuery';
 import { getCurrentDateString } from '../util/dateUtility';
+import { getCombatPower } from '../util/statApi';
 
 export default function CharacterTable({ ocid }) {
   const { data } = useCharacterStatQuery(ocid, getCurrentDateString(-2));
+  console.log(data);
   if (ocid === null || ocid === undefined || ocid.length <= 1) {
     return <div></div>;
   }
 
-  return <div className='text-white text-3xl'>{JSON.stringify(data)}</div>;
+  return (
+    <div className='text-white text-3xl'>전투력 : {getCombatPower(data)}</div>
+  );
 }
