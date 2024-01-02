1a2555f78b2460452c8bd9b4b8bee2122638663a
Jongbum <amjong2@gmail.com>
2024-01-02 23:59:01 +0900

    CharacterInputTable.jsx: party component use six solo component

--- src/components/CharacterInputTable.jsx
+++ src/components/CharacterInputTable.jsx
@@ -25,13 +25,15 @@ export default function CharacterInputTable() {
         <Tab icon={<BoyIcon />} label='1인' sx={{ width: '50%' }} />
         <Tab icon={<GroupsIcon />} label='파티' sx={{ width: '50%' }} />
       </Tabs>
-      <div className='flex w-full h-full items-center justify-center border-solid border-5 border-black'>
+      <div className='w-full h-full m-auto'>
         {value === 0 ? (
           <CharacterInputTableSolo />
         ) : (
           <CharacterInputTableParty />
         )}
       </div>
+      {/* TODO : 보스 계급도 미리 화면 배치 해두고, 버튼 누르면 스크롤 이동을 통해 보여주기 */}
+      <button>잡을 수 있는 보스는?</button>
     </div>
   );
 }
--- src/components/CharacterInputTableSolo.jsx
+++ src/components/CharacterInputTableSolo.jsx
@@ -1,3 +1,5 @@
+import AddIcon from '@mui/icons-material/Add';
+import { IconButton } from '@mui/material';
 import { useCallback, useEffect, useState } from 'react';
 import { getOcidFromNickname } from '../util/openApiManager';
 import InputBar from './InputBar';
@@ -5,6 +7,7 @@ import InputBar from './InputBar';
 export default function CharacterInputTableSolo() {
   const [nickname, setNickname] = useState('');
   const [ocid, setOcid] = useState('');
+  const [isInputBarDisplay, setIsInputBarDisplay] = useState(false);
   const handleSubmit = useCallback((e) => {
     e.preventDefault();
     const inputValue = e.target[0].value;
@@ -22,8 +25,20 @@ export default function CharacterInputTableSolo() {
     });
   }, [nickname]);
   return (
-    <div>
-      <InputBar handleSubmit={handleSubmit}></InputBar>
+    <div className='flex w-2/5 h-4/5 text-center items-center justify-center border-4 border-red-900 my-10 m-auto rounded-xl'>
+      {!isInputBarDisplay && (
+        <IconButton
+          className='hover:scale-150'
+          onClick={() => {
+            setIsInputBarDisplay(true);
+          }}
+        >
+          <AddIcon />
+        </IconButton>
+      )}
+
+      {/* TODO : InputBar -> modal popup */}
+      {isInputBarDisplay && <InputBar handleSubmit={handleSubmit}></InputBar>}
       {ocid && <p>{ocid}</p>}
     </div>
   );
