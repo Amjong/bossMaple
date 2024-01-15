2e1ca3f1dab6a551bd625756770e633617bbac31
Jongbum <amjong2@gmail.com>
2024-01-15 13:55:11 +0900

    Apply custom fonts, colors

--- src/App.js
+++ src/App.js
@@ -20,34 +20,18 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <div className='flex flex-col'>
-        <header className='bg-gray-200 text-red-200 text-3xl font-bold text-center'>
-          BossMaple
-        </header>
-        <section className='bg-gray-200 h-screen flex flex-col'>
-          <p className='h-1/5 font-bold text-3xl text-center flex items-center justify-center'>
-            캐릭터 추가
-          </p>
-          <div className='text-center m-auto w-1/2 h-4/5 border-solid border-black border-2'>
-            {/* <ErrorBoundary fallback={<Error></Error>}>
-              <Suspense fallback={<Loading></Loading>}>
-                <CharacterInfo></CharacterInfo>
-              </Suspense>
-            </ErrorBoundary> */}
-            <ErrorBoundary fallback={<Error></Error>}>
-              <Suspense fallback={<Loading></Loading>}>
-                <StarforceInfo></StarforceInfo>
-              </Suspense>
-            </ErrorBoundary>
-            {/* <ErrorBoundary fallback={<Error></Error>}>
-              <Suspense fallback={<Loading></Loading>}>
-                <CharacterInputTable />
-              </Suspense>
-            </ErrorBoundary> */}
-          </div>
+        <section label='searchArea'>
+          <div className='text-xl'>폰트 테스트!!! 1</div>
+          <div className='text-3xl'>폰트 테스트!!! 2</div>
+          <div className='bg-y1 w-10 h-10 text-y1'>색상 테스트!</div>
+          <div className='bg-y2 w-10 h-10'>색상 테스트!</div>
+          <div className='bg-y4 w-10 h-10'>색상 테스트!</div>
+          <div className='bg-y5 w-10 h-10'>색상 테스트!</div>
+          <div className='bg-r1 w-10 h-10'>색상 테스트!</div>
+          <div className='bg-r2 w-10 h-10'>색상 테스트!</div>
+          <div className='bg-r3 w-10 h-10'>색상 테스트!</div>
+          <div className='bg-r4 w-10 h-10'>색상 테스트!</div>
         </section>
-        <footer className='text-black text-3xl text-center'>
-          Data based on NEXON Open API.
-        </footer>
       </div>
     </QueryClientProvider>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,29 @@ module.exports = {
     './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        '3xl': '48px',
+        '2xl': '36px',
+        xl: '24px',
+        lg: '16px',
+        md: '12px',
+      },
+      colors: {
+        y1: '#FFC700',
+        y2: '#FFD233',
+        y4: '#FFE380',
+        y5: '#FFF4CC',
+        r1: '#D11B00',
+        r2: '#DA4933',
+        r3: '#E88D80',
+        r4: '#F6D1CC',
+      },
+      fontFamily: {
+        regular: ['NexonGothicRegular'],
+        bold: ['NexonGothicBold'],
+      },
+    },
   },
   plugins: [],
 };
