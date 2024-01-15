7f16d0a0f7bef7e3f75db7ba9c59f814b8b11b47
Jongbum <amjong2@gmail.com>
2024-01-15 23:29:32 +0900

    data/itemLevelInfo.js: Update itemLevelInfo

--- src/data/itemLevelInfo.js
+++ src/data/itemLevelInfo.js
@@ -1,10 +1,136 @@
 let itemLevelTable = {};
 
-itemLevelTable['응축된 힘의 결정석'] = 110;
-itemLevelTable['레드 아처 마이스터 심볼'] = 100;
-itemLevelTable['몽환의 벨트'] = 200;
+// 여명
+itemLevelTable['에스텔라 이어링'] = 160;
+itemLevelTable['트와일라이트 마크'] = 140;
+
+// 카루타
+
+itemLevelTable['이글아이 던위치로브'] = 150;
 itemLevelTable['트릭스터 던위치팬츠'] = 150;
+itemLevelTable['하이네스 던위치햇'] = 150;
+
+itemLevelTable['이글아이 어새신셔츠'] = 150;
+itemLevelTable['트릭스터 어새신팬츠'] = 150;
+itemLevelTable['하이네스 어새신보닛'] = 150;
+
+itemLevelTable['이글아이 레인져후드'] = 150;
+itemLevelTable['트릭스터 레인져팬츠'] = 150;
+itemLevelTable['하이네스 레인져베레'] = 150;
+
+itemLevelTable['이글아이 원더러코트'] = 150;
+itemLevelTable['트릭스터 원더러팬츠'] = 150;
+itemLevelTable['하이네스 원더러햇'] = 150;
+
+itemLevelTable['이글아이 워리어아머'] = 150;
+itemLevelTable['트릭스터 워리어팬츠'] = 150;
+itemLevelTable['하이네스 워리어헬름'] = 150;
+
+// 아케인
+
+itemLevelTable['아케인셰이드 메이지글러브'] = 200;
+itemLevelTable['아케인셰이드 메이지숄더'] = 200;
+itemLevelTable['아케인셰이드 메이지슈즈'] = 200;
+itemLevelTable['아케인셰이드 메이지슈트'] = 200;
+itemLevelTable['아케인셰이드 메이지케이프'] = 200;
+itemLevelTable['아케인셰이드 메이지햇'] = 200;
+
+itemLevelTable['아케인셰이드 나이트글러브'] = 200;
+itemLevelTable['아케인셰이드 나이트숄더'] = 200;
+itemLevelTable['아케인셰이드 나이트슈즈'] = 200;
+itemLevelTable['아케인셰이드 나이트슈트'] = 200;
+itemLevelTable['아케인셰이드 나이트케이프'] = 200;
+itemLevelTable['아케인셰이드 나이트햇'] = 200;
+
+itemLevelTable['아케인셰이드 시프글러브'] = 200;
+itemLevelTable['아케인셰이드 시프숄더'] = 200;
+itemLevelTable['아케인셰이드 시프슈즈'] = 200;
+itemLevelTable['아케인셰이드 시프슈트'] = 200;
+itemLevelTable['아케인셰이드 시프케이프'] = 200;
+itemLevelTable['아케인셰이드 시프햇'] = 200;
+
+itemLevelTable['아케인셰이드 아처글러브'] = 200;
+itemLevelTable['아케인셰이드 아처숄더'] = 200;
+itemLevelTable['아케인셰이드 아처슈즈'] = 200;
+itemLevelTable['아케인셰이드 아처슈트'] = 200;
+itemLevelTable['아케인셰이드 아처케이프'] = 200;
+itemLevelTable['아케인셰이드 아처햇'] = 200;
+
+itemLevelTable['아케인셰이드 파이렛글러브'] = 200;
+itemLevelTable['아케인셰이드 파이렛숄더'] = 200;
+itemLevelTable['아케인셰이드 파이렛슈즈'] = 200;
+itemLevelTable['아케인셰이드 파이렛슈트'] = 200;
+itemLevelTable['아케인셰이드 파이렛케이프'] = 200;
+itemLevelTable['아케인셰이드 파이렛햇'] = 200;
+
+// 앱솔
+
+itemLevelTable['앱솔랩스 나이트숄더'] = 160;
+itemLevelTable['앱솔랩스 나이트헬름'] = 160;
+
+// 칠흑
+itemLevelTable['몽환의 벨트'] = 200;
+itemLevelTable['루즈 컨트롤 머신 마크'] = 200;
+itemLevelTable['마력이 깃든 안대'] = 200;
+itemLevelTable['커맨더 포스 이어링'] = 200;
+itemLevelTable['거대한 공포'] = 200;
+itemLevelTable['고통의 근원'] = 200;
+
+// 보장
+itemLevelTable['응축된 힘의 결정석'] = 110;
+itemLevelTable['아쿠아틱 레터 눈장식'] = 100;
+itemLevelTable['블랙빈 마크'] = 135;
+itemLevelTable['파풀라투스 마크'] = 145;
+itemLevelTable['지옥의 불꽃'] = 130;
+itemLevelTable['데아 시두스 이어링'] = 130;
+itemLevelTable['실버블라썸 링'] = 110;
+itemLevelTable['고귀한 이피아의 반지'] = 120;
+itemLevelTable['가디언 엔젤 링'] = 160;
+itemLevelTable['혼테일의 목걸이'] = 120;
+itemLevelTable['카오스 혼테일의 목걸이'] = 120;
+itemLevelTable['매커네이터 펜던트'] = 120;
+itemLevelTable['도미네이터 펜던트'] = 140;
+itemLevelTable['골든 클로버 벨트'] = 140;
+itemLevelTable['분노한 자쿰의 벨트'] = 150;
+itemLevelTable['로얄 블랙메탈 숄더'] = 110;
+
+// 펜살
+itemLevelTable['펜살리르 메이지로브'] = 140;
+itemLevelTable['펜살리르 메이지부츠'] = 140;
+itemLevelTable['펜살리르 메이지샐릿'] = 140;
+itemLevelTable['펜살리르 메이지케이프'] = 140;
+itemLevelTable['펜살리르 메이지글러브'] = 140;
+
 itemLevelTable['펜살리르 배틀메일'] = 140;
+itemLevelTable['펜살리르 배틀글러브'] = 140;
+itemLevelTable['펜살리르 배틀헬름'] = 140;
+itemLevelTable['펜살리르 배틀부츠'] = 140;
+itemLevelTable['펜살리르 배틀케이프'] = 140;
+
+itemLevelTable['펜살리르 센티널글러브'] = 140;
+itemLevelTable['펜살리르 센티널부츠'] = 140;
+itemLevelTable['펜살리르 센티널슈트'] = 140;
+itemLevelTable['펜살리르 센티널케이프'] = 140;
+itemLevelTable['펜살리르 센티널캡'] = 140;
+
+itemLevelTable['펜살리르 스키퍼글러브'] = 140;
+itemLevelTable['펜살리르 스키퍼부츠'] = 140;
+itemLevelTable['펜살리르 스키퍼케이프'] = 140;
+itemLevelTable['펜살리르 스키퍼코트'] = 140;
+itemLevelTable['펜살리르 스키퍼햇'] = 140;
+
+itemLevelTable['펜살리르 체이서글러브'] = 140;
+itemLevelTable['펜살리르 체이서부츠'] = 140;
+itemLevelTable['펜살리르 체이서아머'] = 140;
+itemLevelTable['펜살리르 체이서케이프'] = 140;
+itemLevelTable['펜살리르 체이서햇'] = 140;
+
+// 기타 악세서리
+
+itemLevelTable['레드 아처 마이스터 심볼'] = 100;
+itemLevelTable['샤이니 레드 아처 마이스터 심볼'] = 130;
+itemLevelTable['샤이니 레드 파이렛 마이스터 심볼'] = 130;
+itemLevelTable['마이스터링'] = 140;
 
 const getItemLevelFromTable = (name) => {
   return itemLevelTable[name];
