f4107070b370ed7dce813d4886ad30f2baa9e10d
Jongbum <amjong2@gmail.com>
2024-01-22 19:10:53 +0900

    Navbar.jsx: Add link to logo and Add selected effect to each menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,40 @@
 import { Link } from 'react-router-dom';
 import { logoSrc } from '../data/image/encodedImage';
+import { useState } from 'react';
 
 export default function Navbar() {
+  const [menu, setMenu] = useState(0);
   return (
-    <header className='flex w-full basis-[5.5%] bg-n1 items-center z-10'>
-      <img
-        src={logoSrc}
-        alt='logo'
-        className='w-[126px] h-4/5 translate-x-[50px]'
-      />
+    <header className='flex w-full bg-n1 items-center z-10'>
+      <div className='ml-10'>
+        <Link to='/bossMaple'>
+          <img src={logoSrc} alt='logo' className='w-[126px] h-4/5' />
+        </Link>
+      </div>
       <div className='flex w-full items-center justify-end gap-[36px] pl-[25px] pr-0 py-0 relative translate-x-[-50px]'>
         <div className='inline-flex items-center justify-end gap-[33px] relative flex-[0_0_auto]'>
-          <div className='font-regular font-extrabold text-y4 text-xl relative w-fit mt-[-1.00px] text-center tracking-[0] leading-[normal] whitespace-nowrap'>
-            <Link to='/bossMaple' className='underline underline-offset-4'>
+          <div
+            className={`font-regular ${
+              menu === 0 ? 'text-y4 font-extrabold ' : 'text-white font-medium '
+            } relative w-fit mt-[-1.00px] text-center tracking-[0] leading-[normal] whitespace-nowrap`}
+          >
+            <Link
+              to='/bossMaple'
+              className='underline underline-offset-4'
+              onClick={() => setMenu(0)}
+            >
               스타포스
             </Link>
           </div>
-          <div className='font-regular font-light text-white relative w-fit mt-[-1.00px] text-xl text-center tracking-[0] leading-[normal] whitespace-nowrap'>
+          <div
+            className={`font-regular ${
+              menu === 1 ? 'text-y4 font-extrabold ' : 'text-white font-medium '
+            }relative w-fit mt-[-1.00px] text-center tracking-[0] leading-[normal] whitespace-nowrap`}
+          >
             <Link
               to='/bossMaple/guide'
               className='underline underline-offset-4'
+              onClick={() => setMenu(1)}
             >
               서비스가이드
             </Link>
