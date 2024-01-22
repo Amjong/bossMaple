480fc7a488d611b34aafc4840cd877446f9a3860
Jongbum <amjong2@gmail.com>
2024-01-22 17:50:28 +0900

    page/Guide.jsx: Add tabs to rendering each panel(apikeyguide, qna)

---
+++ src/components/ApiKeyGuidePanel.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function ApiKeyGuidePanel() {
+  return <div>ApiKeyGuide</div>;
+}
--- src/components/page/Guide.jsx
+++ src/components/page/Guide.jsx
@@ -1,5 +1,31 @@
-import React from 'react';
+import { useState } from 'react';
+import ApiKeyGuidePanel from '../ApiKeyGuidePanel';
+import QnaPanel from '../QnaPanel';
+import TabBtns from '../ui/TapBtns';
+
+const tabBtnsArray = [
+  {
+    text: 'API KEY 발급',
+    component: <ApiKeyGuidePanel />,
+  },
+  {
+    text: 'Q&A',
+    component: <QnaPanel />,
+  },
+];
 
 export default function Guide() {
-  return <div>가이드 페이지 입니다.</div>;
+  const [menu, setMenu] = useState(0);
+  const handleChange = (value) => {
+    setMenu(value);
+  };
+  return (
+    <div className='flex flex-col bg-n1 h-screen'>
+      <div className='ml-10 mt-5'>
+        <TabBtns onChanged={handleChange} tabBtnsArray={tabBtnsArray} />
+      </div>
+
+      <div className='m-10 bg-n2 h-full'>{tabBtnsArray[menu].component}</div>
+    </div>
+  );
 }
