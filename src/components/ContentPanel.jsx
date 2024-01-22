d898349ca0f87d6622ac7962d3c174107c05de65
Jongbum <amjong2@gmail.com>
2024-01-22 14:21:04 +0900

    ContentPanel.jsx: Add API KEY enter msg when API KEY is not entered

--- src/components/ContentPanel.jsx
+++ src/components/ContentPanel.jsx
@@ -3,6 +3,7 @@ import UsedMesoPanel from './UsedMesoPanel';
 import SuccessRatePanel from './SuccessRatePanel';
 import HistoryPanel from './HistoryPanel';
 import TapBtns from './ui/TapBtns';
+import { useStarforceInfoArray } from '../context/starforceInfoContext';
 
 const contentArray = [
   {
@@ -18,13 +19,27 @@ const contentArray = [
 
 export default function ContentPanel() {
   const [menu, setMenu] = useState(0);
+  const [starforceInfoArray] = useStarforceInfoArray();
   const handleChange = (value) => {
     setMenu(value);
   };
   return (
     <div className='w-full'>
       <TapBtns onChanged={handleChange} />
-      <div className='mt-10'>{contentArray[menu].component}</div>
+      <div className='mt-10'>
+        {starforceInfoArray.length === 0 ? (
+          <div className='text-[36px] font-bold text-center text-white mt-20 leading-loose'>
+            API KEY 값을 입력 해주세요.
+            <br />
+            <a href='/guide' className='text-r2 underline underline-offset-4'>
+              API KEY 값 입력 가이드
+            </a>{' '}
+            보러가기
+          </div>
+        ) : (
+          contentArray[menu].component
+        )}
+      </div>
     </div>
   );
 }
