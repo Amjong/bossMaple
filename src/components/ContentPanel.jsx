7ddcd7cdff194652d0e882edc02df1d17de5e519
Jongbum <amjong2@gmail.com>
2024-01-22 17:23:36 +0900

    ui/TapBtns.jsx: Seperate text array to props

--- src/components/ContentPanel.jsx
+++ src/components/ContentPanel.jsx
@@ -5,15 +5,19 @@ import HistoryPanel from './HistoryPanel';
 import TapBtns from './ui/TapBtns';
 import { useStarforceInfoArray } from '../context/starforceInfoContext';
 import { Link } from 'react-router-dom';
+import { useLoading } from '../context/loadingContext';
 
 const contentArray = [
   {
+    text: '사용 메소량',
     component: <UsedMesoPanel />,
   },
   {
+    text: '강화 단계 별 성공률',
     component: <SuccessRatePanel />,
   },
   {
+    text: '히스토리',
     component: <HistoryPanel />,
   },
 ];
@@ -21,14 +25,15 @@ const contentArray = [
 export default function ContentPanel() {
   const [menu, setMenu] = useState(0);
   const [starforceInfoArray] = useStarforceInfoArray();
+  const [isLoading] = useLoading();
   const handleChange = (value) => {
     setMenu(value);
   };
   return (
     <div className='w-full'>
-      <TapBtns onChanged={handleChange} />
+      <TapBtns onChanged={handleChange} tabBtnsArray={contentArray} />
       <div className='mt-10'>
-        {starforceInfoArray.length === 0 ? (
+        {!isLoading && starforceInfoArray.length === 0 ? (
           <div className='text-[36px] font-bold text-center text-white mt-20 leading-loose'>
             API KEY 값을 입력 해주세요.
             <br />
--- src/components/ui/TapBtns.jsx
+++ src/components/ui/TapBtns.jsx
@@ -1,22 +1,10 @@
 import { useState } from 'react';
 import TapBtn from './TapBtn';
-
-const tabBtnsArray = [
-  {
-    text: '사용 메소량',
-  },
-  {
-    text: '강화 단계별 성공률',
-  },
-  {
-    text: '히스토리',
-  },
-];
-export default function TapBtns({ onChanged }) {
+export default function TapBtns({ onChanged, tabBtnsArray }) {
   const [selectedValue, setSelectedValue] = useState(0);
   return (
     <div className='w-full flex justify-start gap-3'>
-      {tabBtnsArray.map((tabBtn, index) => {
+      {tabBtnsArray?.map(({ text }, index) => {
         return (
           <TapBtn
             handleClick={() => {
@@ -24,7 +12,7 @@ export default function TapBtns({ onChanged }) {
               onChanged(index);
             }}
             isSelected={selectedValue === index}
-            text={tabBtn.text}
+            text={text}
           />
         );
       })}
