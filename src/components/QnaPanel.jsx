df94ccc6c3ae644a73c2daff97a8dba4a73078e0
Jongbum <amjong2@gmail.com>
2024-01-28 23:35:54 +0900

    QnaPanel.jsx: Fix default msg of QnaPanel

--- src/components/QnaPanel.jsx
+++ src/components/QnaPanel.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 
 export default function QnaPanel() {
-  return <div className='text-3xl text-white'> Q&A 현재 미지원 입니다. </div>;
+  return (
+    <div className='text-2xl text-white h-screen text-center'>
+      <br />
+      Q&A는 곧 업데이트 될 예정입니다.{' '}
+    </div>
+  );
 }
