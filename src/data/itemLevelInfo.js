8729eb59a4a41d7ba5165d5b6775c8ea6bfee657
Jongbum <amjong2@gmail.com>
2024-01-29 20:37:16 +0900

    Update itemLevelInfo

--- src/data/itemLevelInfo.js
+++ src/data/itemLevelInfo.js
@@ -3,6 +3,8 @@ let itemLevelTable = {};
 // 여명
 itemLevelTable['에스텔라 이어링'] = 160;
 itemLevelTable['트와일라이트 마크'] = 140;
+itemLevelTable['데이브레이크 펜던트'] = 140;
+itemLevelTable['여명의 가디언 엔젤 링'] = 160;
 
 // 카루타
 
@@ -25,6 +27,39 @@ itemLevelTable['하이네스 원더러햇'] = 150;
 itemLevelTable['이글아이 워리어아머'] = 150;
 itemLevelTable['트릭스터 워리어팬츠'] = 150;
 itemLevelTable['하이네스 워리어헬름'] = 150;
+
+itemLevelTable['파프니르 미스틸테인'] = 150;
+itemLevelTable['파프니르 트윈클리버'] = 150;
+itemLevelTable['파프니르 골디언해머'] = 150;
+itemLevelTable['파프니르 페니텐시아'] = 150;
+itemLevelTable['파프니르 배틀클리버'] = 150;
+itemLevelTable['파프니르 라이트닝어'] = 150;
+itemLevelTable['파프니르 브류나크'] = 150;
+itemLevelTable['파프니르 문글레이브'] = 150;
+itemLevelTable['파프니르 데스브링어'] = 150;
+itemLevelTable['파프니르 빅 마운틴'] = 150;
+itemLevelTable['파프니르 포기브니스'] = 150;
+itemLevelTable['파프니르 마나테이커'] = 150;
+itemLevelTable['파프니르 마나크라운'] = 150;
+itemLevelTable['파프니르 마나크래들'] = 150;
+itemLevelTable['파프니르 ESP리미터'] = 150;
+itemLevelTable['파프니르 매직 건틀렛'] = 150;
+itemLevelTable['파프니르 윈드체이서'] = 150;
+itemLevelTable['파프니르 윈드윙슈터'] = 150;
+itemLevelTable['파프니르 듀얼윈드윙'] = 150;
+itemLevelTable['파프니르 에인션트 보우'] = 150;
+itemLevelTable['파프니르 나이트체이서'] = 150;
+itemLevelTable['파프니르 다마스커스'] = 150;
+itemLevelTable['파프니르 리스크홀더'] = 150;
+itemLevelTable['파프니르 클레르시엘'] = 150;
+itemLevelTable['파프니르 스플릿엣지'] = 150;
+itemLevelTable['파프니르 체인'] = 150;
+itemLevelTable['파프니르 용선'] = 150;
+itemLevelTable['파프니르 차크람'] = 150;
+itemLevelTable['파프니르 래피드엣지'] = 150;
+itemLevelTable['파프니르 첼리스카'] = 150;
+itemLevelTable['파프니르 펜리르탈론'] = 150;
+itemLevelTable['파프니르 엔젤릭슈터'] = 150;
 
 // 아케인
 
@@ -63,18 +98,201 @@ itemLevelTable['아케인셰이드 파이렛슈트'] = 200;
 itemLevelTable['아케인셰이드 파이렛케이프'] = 200;
 itemLevelTable['아케인셰이드 파이렛햇'] = 200;
 
+itemLevelTable['아케인셰이드 세이버'] = 200;
+itemLevelTable['아케인셰이드 엑스'] = 200;
+itemLevelTable['아케인셰이드 해머'] = 200;
+itemLevelTable['아케인셰이드 투핸드소드'] = 200;
+itemLevelTable['아케인셰이드 투핸드엑스'] = 200;
+itemLevelTable['아케인셰이드 투핸드해머'] = 200;
+itemLevelTable['아케인셰이드 스피어'] = 200;
+itemLevelTable['아케인셰이드 폴암'] = 200;
+itemLevelTable['아케인셰이드 데스페라도'] = 200;
+itemLevelTable['아케인셰이드 엘라하'] = 200;
+itemLevelTable['아케인셰이드 튜너'] = 200;
+itemLevelTable['아케인셰이드 완드'] = 200;
+itemLevelTable['아케인셰이드 스태프'] = 200;
+itemLevelTable['아케인셰이드 샤이닝로드'] = 200;
+itemLevelTable['아케인셰이드 ESP리미터'] = 200;
+itemLevelTable['아케인셰이드 매직 건틀렛'] = 200;
+itemLevelTable['아케인셰이드 보우'] = 200;
+itemLevelTable['아케인셰이드 크로스보우'] = 200;
+itemLevelTable['아케인셰이드 듀얼보우건'] = 200;
+itemLevelTable['아케인셰이드 에인션트 보우'] = 200;
+itemLevelTable['아케인셰이드 브레스 슈터'] = 200;
+itemLevelTable['아케인셰이드 대거'] = 200;
+itemLevelTable['아케인셰이드 가즈'] = 200;
+itemLevelTable['아케인셰이드 케인'] = 200;
+itemLevelTable['아케인셰이드 에너지체인'] = 200;
+itemLevelTable['아케인셰이드 초선'] = 200;
+itemLevelTable['아케인셰이드 블레이드'] = 200;
+itemLevelTable['아케인셰이드 피스톨'] = 200;
+itemLevelTable['아케인셰이드 클로'] = 200;
+itemLevelTable['아케인셰이드 시즈건'] = 200;
+itemLevelTable['아케인셰이드 소울슈터'] = 200;
+
 // 앱솔
 
 itemLevelTable['앱솔랩스 나이트숄더'] = 160;
 itemLevelTable['앱솔랩스 나이트헬름'] = 160;
+itemLevelTable['앱솔랩스 나이트슈트'] = 160;
+itemLevelTable['앱솔랩스 나이트슈즈'] = 160;
+itemLevelTable['앱솔랩스 나이트글러브'] = 160;
+itemLevelTable['앱솔랩스 나이트케이프'] = 160;
+itemLevelTable['앱솔랩스 세이버'] = 160;
+itemLevelTable['앱솔랩스 엑스'] = 160;
+itemLevelTable['앱솔랩스 비트해머'] = 160;
+itemLevelTable['앱솔랩스 브로드세이버'] = 160;
+itemLevelTable['앱솔랩스 브로드엑스'] = 160;
+itemLevelTable['앱솔랩스 피어싱스피어'] = 160;
+itemLevelTable['앱솔랩스 핼버드'] = 160;
+itemLevelTable['앱솔랩스 데스페라도'] = 160;
+itemLevelTable['앱솔랩스 파일 갓'] = 160;
+itemLevelTable['앱솔랩스 튜너'] = 160;
+itemLevelTable['앱솔랩스 스펠링완드'] = 160;
+itemLevelTable['앱솔랩스 스펠링스태프'] = 160;
+itemLevelTable['앱솔랩스 샤이닝로드'] = 160;
+itemLevelTable['앱솔랩스 ESP리미터'] = 160;
+itemLevelTable['앱솔랩스 매직 건틀렛'] = 160;
+itemLevelTable['앱솔랩스 메이지크라운'] = 160;
+itemLevelTable['앱솔랩스 메이지슈트'] = 160;
+itemLevelTable['앱솔랩스 메이지슈즈'] = 160;
+itemLevelTable['앱솔랩스 메이지글러브'] = 160;
+itemLevelTable['앱솔랩스 메이지케이프'] = 160;
+itemLevelTable['앱솔랩스 메이지숄더'] = 160;
+itemLevelTable['앱솔랩스 슈팅보우'] = 160;
+itemLevelTable['앱솔랩스 크로스보우'] = 160;
+itemLevelTable['앱솔랩스 듀얼보우건'] = 160;
+itemLevelTable['앱솔랩스 에인션트 보우'] = 160;
+itemLevelTable['앱솔랩스 브레스 슈터'] = 160;
+itemLevelTable['앱솔랩스 아처후드'] = 160;
+itemLevelTable['앱솔랩스 아처슈트'] = 160;
+itemLevelTable['앱솔랩스 아처슈즈'] = 160;
+itemLevelTable['앱솔랩스 아처글러브'] = 160;
+itemLevelTable['앱솔랩스 아처케이프'] = 160;
+itemLevelTable['앱솔랩스 아처숄더'] = 160;
+itemLevelTable['앱솔랩스 슬래셔'] = 160;
+itemLevelTable['앱솔랩스 리벤지가즈'] = 160;
+itemLevelTable['앱솔랩스 핀쳐케인'] = 160;
+itemLevelTable['앱솔랩스 에너지소드'] = 160;
+itemLevelTable['앱솔랩스 체인'] = 160;
+itemLevelTable['앱솔랩스 괴선'] = 160;
+itemLevelTable['앱솔랩스 차크람'] = 160;
+itemLevelTable['앱솔랩스 블레이드'] = 160;
+itemLevelTable['앱솔랩스 시프캡'] = 160;
+itemLevelTable['앱솔랩스 시프슈트'] = 160;
+itemLevelTable['앱솔랩스 시프슈즈'] = 160;
+itemLevelTable['앱솔랩스 시프글러브'] = 160;
+itemLevelTable['앱솔랩스 시프케이프'] = 160;
+itemLevelTable['앱솔랩스 시프숄더'] = 160;
+itemLevelTable['앱솔랩스 포인팅건'] = 160;
+itemLevelTable['앱솔랩스 블로우너클'] = 160;
+itemLevelTable['앱솔랩스 블래스트캐논'] = 160;
+itemLevelTable['앱솔랩스 소울슈터'] = 160;
+itemLevelTable['앱솔랩스 파이렛페도라'] = 160;
+itemLevelTable['앱솔랩스 파이렛슈트'] = 160;
+itemLevelTable['앱솔랩스 파이렛슈즈'] = 160;
+itemLevelTable['앱솔랩스 파이렛글러브'] = 160;
+itemLevelTable['앱솔랩스 파이렛케이프'] = 160;
+itemLevelTable['앱솔랩스 파이렛숄더'] = 160;
+
+// 여제
+itemLevelTable['라이온하트 커틀러스'] = 140;
+itemLevelTable['라이온하트 챔피온엑스'] = 140;
+itemLevelTable['라이온하트 배틀해머'] = 140;
+itemLevelTable['라이온하트 배틀시미터'] = 140;
+itemLevelTable['라이온하트 배틀엑스'] = 140;
+itemLevelTable['라이온하트 기간틱모울'] = 140;
+itemLevelTable['라이온하트 푸스키나'] = 140;
+itemLevelTable['라이온하트 파르티잔'] = 140;
+itemLevelTable['라이온하트 페인풀 데스티니'] = 140;
+itemLevelTable['라이온하트 발로르'] = 140;
+itemLevelTable['라이온하트 리스트레인트'] = 140;
+itemLevelTable['라이온하트 배틀헬름'] = 140;
+itemLevelTable['라이온하트 배틀메일'] = 140;
+itemLevelTable['라이온하트 배틀브레이서'] = 140;
+itemLevelTable['라이온하트 배틀부츠'] = 140;
+itemLevelTable['라이온하트 배틀케이프'] = 140;
+itemLevelTable['라이온하트 배틀숄더'] = 140;
+itemLevelTable['드래곤테일 아크완드'] = 140;
+itemLevelTable['드래곤테일 워스태프'] = 140;
+itemLevelTable['드래곤테일 타나토스'] = 140;
+itemLevelTable['드래곤테일 ESP리미터'] = 140;
+itemLevelTable['드래곤테일 매직건틀렛'] = 140;
+itemLevelTable['드래곤테일 메이지샐릿'] = 140;
+itemLevelTable['드래곤테일 메이지로브'] = 140;
+itemLevelTable['드래곤테일 메이지피스트'] = 140;
+itemLevelTable['드래곤테일 메이지슈즈'] = 140;
+itemLevelTable['드래곤테일 메이지케이프'] = 140;
+itemLevelTable['드래곤테일 메이지숄더'] = 140;
+itemLevelTable['팔콘윙 컴포지트보우'] = 140;
+itemLevelTable['팔콘윙 헤비크로스보우'] = 140;
+itemLevelTable['팔콘윙 그란 듀얼보우건'] = 140;
+itemLevelTable['팔콘윙 에인션트 보우'] = 140;
+itemLevelTable['팔콘윙 스플린디드네로'] = 140;
+itemLevelTable['팔콘윙 센티널캡'] = 140;
+itemLevelTable['팔콘윙 센티널슈트'] = 140;
+itemLevelTable['팔콘윙 센티널글러브'] = 140;
+itemLevelTable['팔콘윙 센티널부츠'] = 140;
+itemLevelTable['팔콘윙 센티널케이프'] = 140;
+itemLevelTable['팔콘윙 센티널숄더'] = 140;
+itemLevelTable['레이븐혼 체이서햇'] = 140;
+itemLevelTable['레이븐혼 체이서아머'] = 140;
+itemLevelTable['레이븐혼 체이서글러브'] = 140;
+itemLevelTable['레이븐혼 체이서부츠'] = 140;
+itemLevelTable['레이븐혼 체이서케이프'] = 140;
+itemLevelTable['레이븐혼 체이서숄더'] = 140;
+itemLevelTable['레이븐혼 바젤라드'] = 140;
+itemLevelTable['레이븐혼 메탈피스트'] = 140;
+itemLevelTable['레이븐혼 섀도우블레이드'] = 140;
+itemLevelTable['레이븐혼 크림슨 케인'] = 140;
+itemLevelTable['레이븐혼 퀸스폴링'] = 140;
+itemLevelTable['레이븐혼 체인'] = 140;
+itemLevelTable['레이븐혼 각선'] = 140;
+itemLevelTable['샤크투스 샤프슈터'] = 140;
+itemLevelTable['샤크투스 와일드탈론'] = 140;
+itemLevelTable['샤크투스 플람'] = 140;
+itemLevelTable['샤크투스 소울드링커'] = 140;
+itemLevelTable['샤크투스 퀸스폴링'] = 140;
+itemLevelTable['샤크투스 스키퍼햇'] = 140;
+itemLevelTable['샤크투스 스키퍼코트'] = 140;
+itemLevelTable['샤크투스 스키퍼글러브'] = 140;
+itemLevelTable['샤크투스 스키퍼부츠'] = 140;
+itemLevelTable['샤크투스 스키퍼케이프'] = 140;
+itemLevelTable['샤크투스 스키퍼숄더'] = 140;
+
+// 에테르넬
+itemLevelTable['에테르넬 나이트헬름'] = 250;
+itemLevelTable['에테르넬 나이트아머'] = 250;
+itemLevelTable['에테르넬 나이트팬츠'] = 250;
+itemLevelTable['에테르넬 나이트숄더'] = 250;
+
+itemLevelTable['에테르넬 메이지햇'] = 250;
+itemLevelTable['에테르넬 메이지로브'] = 250;
+itemLevelTable['에테르넬 메이지팬츠'] = 250;
+itemLevelTable['에테르넬 메이지숄더'] = 250;
+
+itemLevelTable['에테르넬 아처햇'] = 250;
+itemLevelTable['에테르넬 아처후드'] = 250;
+itemLevelTable['에테르넬 아처팬츠'] = 250;
+itemLevelTable['에테르넬 아처숄더'] = 250;
+
+itemLevelTable['에테르넬 시프반다나'] = 250;
+itemLevelTable['에테르넬 시프셔츠'] = 250;
+itemLevelTable['에테르넬 시프팬츠'] = 250;
+itemLevelTable['에테르넬 시프숄더'] = 250;
+
+itemLevelTable['에테르넬 파이렛햇'] = 250;
+itemLevelTable['에테르넬 파이렛코트'] = 250;
+itemLevelTable['에테르넬 파이렛팬츠'] = 250;
+itemLevelTable['에테르넬 파이렛숄더'] = 250;
 
 // 칠흑
 itemLevelTable['몽환의 벨트'] = 200;
-itemLevelTable['루즈 컨트롤 머신 마크'] = 200;
-itemLevelTable['마력이 깃든 안대'] = 200;
+itemLevelTable['루즈 컨트롤 머신 마크'] = 160;
+itemLevelTable['마력이 깃든 안대'] = 160;
 itemLevelTable['커맨더 포스 이어링'] = 200;
 itemLevelTable['거대한 공포'] = 200;
-itemLevelTable['고통의 근원'] = 200;
+itemLevelTable['고통의 근원'] = 160;
 
 // 보장
 itemLevelTable['응축된 힘의 결정석'] = 110;
@@ -125,12 +343,48 @@ itemLevelTable['펜살리르 체이서아머'] = 140;
 itemLevelTable['펜살리르 체이서케이프'] = 140;
 itemLevelTable['펜살리르 체이서햇'] = 140;
 
+// 마이스터
+
+itemLevelTable['마이스터링'] = 140;
+itemLevelTable['마이스터 이어링'] = 140;
+itemLevelTable['마이스터 숄더'] = 140;
+itemLevelTable['마이스터링'] = 140;
+
+// 라이징 썬
+
+itemLevelTable['라이징 썬 펜던트'] = 130;
+
 // 기타 악세서리
 
 itemLevelTable['레드 아처 마이스터 심볼'] = 100;
 itemLevelTable['샤이니 레드 아처 마이스터 심볼'] = 130;
 itemLevelTable['샤이니 레드 파이렛 마이스터 심볼'] = 130;
-itemLevelTable['마이스터링'] = 140;
+itemLevelTable['샤이니 레드 매지션 마이스터 심볼'] = 130;
+itemLevelTable['샤이니 레드 시프 마이스터 심볼'] = 130;
+itemLevelTable['샤이니 레드 워리어 마이스터 심볼'] = 130;
+itemLevelTable['샤이니 레드 스페셜 심볼'] = 130;
+itemLevelTable['베어스 퍼플 펜던트'] = 130;
+itemLevelTable['아울스 퍼플 펜던트'] = 130;
+itemLevelTable['울프스 퍼플 펜던트'] = 130;
+itemLevelTable['피콕스 퍼플 펜던트'] = 130;
+itemLevelTable['피어리스 펜던트'] = 130;
+
+itemLevelTable['강인함의 익스트림 벨트'] = 130;
+itemLevelTable['날카로운 익스트림 벨트'] = 130;
+itemLevelTable['지혜의 익스트림 벨트'] = 130;
+itemLevelTable['행운의 익스트림 벨트'] = 130;
+
+itemLevelTable['날카로운 샤이니 로얄 마이스터 숄더'] = 130;
+itemLevelTable['앤티크 루트 숄더'] = 150;
+itemLevelTable['앤티크 리프 숄더'] = 130;
+
+// 슈페리얼
+
+itemLevelTable['타일런트 리카온 벨트'] = 150;
+itemLevelTable['타일런트 알테어 벨트'] = 150;
+itemLevelTable['타일런트 케이론 벨트'] = 150;
+itemLevelTable['타일런트 헤르메스 벨트'] = 150;
+itemLevelTable['타일런트 히아데스 벨트'] = 140;
 
 const getItemLevelFromTable = (name) => {
   return itemLevelTable[name];
