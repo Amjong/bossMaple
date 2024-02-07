05cb59d76c0fc3d57661e583a1af3e0557787725
Jongbum <amjong2@gmail.com>
2024-02-08 00:25:08 +0900

    page/Root.jsx: Move context to Root (fixed `UMUV-5` `UMUV-6`)

--- src/components/page/Home.jsx
+++ src/components/page/Home.jsx
@@ -1,22 +1,9 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import PeriodSelectPanel from '../PeriodSelectPanel';
 import ApiKeyInputPanel from '../ApiKeyInputPanel';
 import AvatarPanel from '../AvatarPanel';
 import ContentPanel from '../ContentPanel';
-import { StarforceProvider } from '../../context/starforceInfoContext';
-import { UserInfoProvider } from '../../context/userInfoContext';
-import { LoadingProvider } from '../../context/loadingContext';
 import UpBtn from '../ui/UpBtn';
 import { useEffect, useState } from 'react';
-import { ContentErrorProvider } from '../../context/contentErrorContext';
-
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      suspense: true,
-    },
-  },
-});
 
 const scrollToTop = () => {
   window.scrollTo({
@@ -42,49 +29,39 @@ export default function Home() {
     return () => window.removeEventListener('scroll', toggleVisibility);
   }, []);
   return (
-    <QueryClientProvider client={queryClient}>
-      <StarforceProvider>
-        <UserInfoProvider>
-          <div className='flex flex-col h-full'>
-            <LoadingProvider>
-              <ContentErrorProvider>
-                <section
-                  label='searchArea'
-                  className='flex bg-custom bg-fixed bg-cover w-full pb-5'
-                >
-                  <div className='pt-10 pl-10 pr-10 min-w-[400px] sm:hidden block ml-10'>
-                    <AvatarPanel />
-                  </div>
-                  <div className='flex flex-col mt-10 w-full sm:flex-col-reverse'>
-                    <div>
-                      <ApiKeyInputPanel />
-                    </div>
-                    <div className='sm:mb-20'>
-                      <PeriodSelectPanel />
-                    </div>
-                  </div>
-                </section>
-                <section
-                  label='contentArea'
-                  className='overflow-auto bg-n1 flex flex-col lg:min-h-[700px] w-full'
-                >
-                  <div className='px-[80px] sm:pl-[5px] py-[36.96px] w-full'>
-                    <ContentPanel />
-                  </div>
-                </section>
-                {isVisible && (
-                  <div
-                    className='fixed bottom-[100px] right-[35px]'
-                    onClick={scrollToTop}
-                  >
-                    <UpBtn />
-                  </div>
-                )}
-              </ContentErrorProvider>
-            </LoadingProvider>
+    <div className='flex flex-col h-full'>
+      <section
+        label='searchArea'
+        className='flex bg-custom bg-fixed bg-cover w-full pb-5'
+      >
+        <div className='pt-10 pl-10 pr-10 min-w-[400px] sm:hidden block ml-10'>
+          <AvatarPanel />
+        </div>
+        <div className='flex flex-col mt-10 w-full sm:flex-col-reverse'>
+          <div>
+            <ApiKeyInputPanel />
           </div>
-        </UserInfoProvider>
-      </StarforceProvider>
-    </QueryClientProvider>
+          <div className='sm:mb-20'>
+            <PeriodSelectPanel />
+          </div>
+        </div>
+      </section>
+      <section
+        label='contentArea'
+        className='overflow-auto bg-n1 flex flex-col lg:min-h-[700px] w-full'
+      >
+        <div className='px-[80px] sm:pl-[5px] py-[36.96px] w-full'>
+          <ContentPanel />
+        </div>
+      </section>
+      {isVisible && (
+        <div
+          className='fixed bottom-[100px] right-[35px]'
+          onClick={scrollToTop}
+        >
+          <UpBtn />
+        </div>
+      )}
+    </div>
   );
 }
--- src/components/page/Root.jsx
+++ src/components/page/Root.jsx
@@ -1,13 +1,25 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from '../Navbar';
 import Footer from '../Footer';
+import { StarforceProvider } from '../../context/starforceInfoContext';
+import { UserInfoProvider } from '../../context/userInfoContext';
+import { LoadingProvider } from '../../context/loadingContext';
+import { ContentErrorProvider } from '../../context/contentErrorContext';
 
 export default function Root() {
   return (
     <div className='leading-loose overflow-hidden'>
-      <Navbar />
-      <Outlet />
-      <Footer />
+      <StarforceProvider>
+        <UserInfoProvider>
+          <LoadingProvider>
+            <ContentErrorProvider>
+              <Navbar />
+              <Outlet />
+              <Footer />
+            </ContentErrorProvider>
+          </LoadingProvider>
+        </UserInfoProvider>
+      </StarforceProvider>
     </div>
   );
 }
